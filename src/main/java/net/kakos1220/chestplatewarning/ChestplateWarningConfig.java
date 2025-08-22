e3609247ee698535c9e0cfa695520c8766815869
KAkos1220 <KAkosyyy@gmail.com>
2025-08-23 01:19:03 +0200

    Final touches.

--- src/main/java/net/kakos1220/chestplatewarning/ChestplateWarningConfig.java
+++ src/main/java/net/kakos1220/chestplatewarning/ChestplateWarningConfig.java
@@ -83,6 +83,18 @@ public class ChestplateWarningConfig implements ModMenuApi, ConfigScreenFactory<
 
         general.addEntry(entryBuilder
                 .startBooleanToggle(
+                        Text.translatable("config.considerUnbreaking"), ChestplateWarning.considerUnbreaking
+                )
+                .setDefaultValue(true)
+                .setYesNoTextSupplier(value -> value
+                        ? Text.translatable("config.on").formatted(Formatting.GREEN)
+                        : Text.translatable("config.off").formatted(Formatting.RED)
+                )
+                .setSaveConsumer((value) -> ChestplateWarning.considerUnbreaking = value)
+                .build());
+
+        general.addEntry(entryBuilder
+                .startBooleanToggle(
                         Text.translatable("config.considerGameMode"), ChestplateWarning.considerGameMode
                 )
                 .setTooltip(Text.translatable("config.considerGameMode.tooltip"))
@@ -92,18 +104,6 @@ public class ChestplateWarningConfig implements ModMenuApi, ConfigScreenFactory<
                         : Text.translatable("config.off").formatted(Formatting.RED)
                 )
                 .setSaveConsumer((value) -> ChestplateWarning.considerGameMode = value)
-                .build());
-
-        general.addEntry(entryBuilder
-                .startBooleanToggle(
-                        Text.translatable("config.considerUnbreaking"), ChestplateWarning.considerUnbreaking
-                )
-                .setDefaultValue(true)
-                .setYesNoTextSupplier(value -> value
-                        ? Text.translatable("config.on").formatted(Formatting.GREEN)
-                        : Text.translatable("config.off").formatted(Formatting.RED)
-                )
-                .setSaveConsumer((value) -> ChestplateWarning.considerUnbreaking = value)
                 .build());
 
         general.addEntry(entryBuilder
